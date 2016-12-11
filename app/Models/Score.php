8f751584aa192d798d09393656e39e22c55fc308
andytan2624 <andy.tan2624@gmail.com>
2016-12-12 00:20:32 +1100

    Fixed bug in Score to save attendance

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -47,6 +47,7 @@ class Score extends Model
 
         $meeting_half = 1;
         foreach ($input as $key => $value) {
+
             /**
              * If we hit the field toastmaster_2, we're in the second half
              */
@@ -64,6 +65,10 @@ class Score extends Model
             // Change key to toastmaster if its toastmaster_1 or toastmaster_2
             $new_key = in_array($key, config('constants.toastmaster_alias_array')) ? config('constants.toastmaster_slug') : $key;
 
+            if (isset($pointsData[$new_key]) && $pointsData[$new_key]->point_value == 0) {
+                dd($key);
+            }
+
             if (isset($pointsData[$new_key]) && $value != "") {
                 $score->user_id = $value;
                 $score->notes = $new_key == config('constants.grammarian_slug') ? $input[config('constants.word_of_the_day_slug')] : '';
@@ -79,9 +84,13 @@ class Score extends Model
                     $ids = parseIDString($value, "|");
                     // Create a new score for each one
                     foreach ($ids as $id) {
-                        $score->user_id = $id;
-                        $score->point_id = $pointsData[$slug]->id;
-                        $score->point_value = $pointsData[$slug]->point_value;
+                        $newScore = new Score($input);
+                        $newScore->created_by = Auth::user()->id;
+                        $newScore->which_half = $meeting_half;
+                        $newScore->user_id = $id;
+                        $newScore->point_id = $pointsData[$slug]->id;
+                        $newScore->point_value = $pointsData[$slug]->point_value;
+                        $newScore->save();
                     }
                 }
             }
@@ -126,6 +135,7 @@ class Score extends Model
                 $score->point_value = $pointsData[config('constants.speech_evaluator_point_slug')]->point_value;
             }
 
+            // Save score if it hasn't been saved
             if (isset($score->point_id)) {
                 $score->save();
             }
